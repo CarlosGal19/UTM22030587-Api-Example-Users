a5cfa74d515ca09c24790f5c90e095bb7bab90bf
Carlos Alejandro Galindo Islas <utm22030587@utma.edu.mx>
2024-03-25 12:56:58 -0600

    feat: Add authentication using auth0

--- index.js
+++ index.js
@@ -2,11 +2,37 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
+const { auth, requiresAuth } = require('express-openid-connect');
 const { getUsers, createUser } = require("./queries/user.queries");
 
 app.use(bodyParser.json());
 
-app.get("/user", (req, res) => {
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  baseURL: 'http://localhost:3000',
+  clientID: '4xTLaixh8hxWWxdCzlE1NP3tTYlhKkfN',
+  issuerBaseURL: 'https://dev-onyfbauj3gubi6i8.us.auth0.com',
+  secret: 'LONG_RANDOM_STRING'
+};
+
+// The `auth` router attaches /login, /logout
+// and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.oidc.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(
+    req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out'
+  )
+});
+
+// The /profile route will show the user profile as JSON
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user, null, 2));
+});
+
+app.get("/user", requiresAuth(), (req, res) => {
   getUsers(req, res);
 });
 
